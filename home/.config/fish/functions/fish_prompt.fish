6e50d5e68d118e934eb8ce594852c8a606600196
Rob Rix <rob.rix@me.com>
2019-06-29 11:37:15 -0400

    Add a nice, pretty prompt.

---
+++ home/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,19 @@
+function fish_prompt
+	set -l last_status $status
+	set -l colour
+	if not test $last_status -eq 0
+		set colour $fish_color_error
+	else
+		set colour magenta
+	end
+	set -l suffix
+	set suffix '❱'
+
+	set_color -b $colour
+	printf "%*s\r%s\n" (tput cols) (__fish_git_prompt) (prompt_pwd)
+	set_color -b normal
+	set_color $colour
+	echo -n $suffix
+	echo -n ' '
+	set_color normal
+end
