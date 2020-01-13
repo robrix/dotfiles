c44737d65f9ec91b2ca654895dade154138b4626
Rob Rix <rob.rix@me.com>
2020-01-13 12:38:09 -0500

    Allow the number of columns to be passed into the script.

--- bin/ghci-prompt.sh
+++ bin/ghci-prompt.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-printf "%*s\r%s" "$(tput cols)" "$(git describe --contains --all HEAD)" "$@"
+case $1 in
+[1-9][0-9]*)
+  cols="$1"
+  shift
+  ;;
+*)
+  cols="$(tput cols)"
+  ;;
+esac
+
+branch="$(git describe --contains --all HEAD)"
+
+printf "%*s\r%s" "$cols" "${branch:+($branch)}" "$@"
