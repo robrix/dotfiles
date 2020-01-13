85a9bf279db07ae009afc9c01cdc147f8e28b513
Rob Rix <rob.rix@me.com>
2020-01-13 11:53:18 -0500

    Do most of the basic prompt in the ghci script.

--- bin/ghci-prompt.sh
+++ bin/ghci-prompt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-printf "\e[46m\0%*s\r%s\e[m\0\n\e[1;36m\0λ \e[m\0" "$(tput cols)" "$(git describe --contains --all HEAD)" ""
+printf "%*s\r%s" "$(tput cols)" "$(git describe --contains --all HEAD)" ""
